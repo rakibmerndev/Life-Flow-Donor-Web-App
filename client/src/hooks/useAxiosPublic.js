4b283ba68ce970ae9cb3f7528a9b92f14782f7e4
Rakib <abdullahalrakib30@gmail.com>
2026-09-24 04:23:33 +0600

    added filtering on backend and pagination on request page

--- client/src/hooks/useAxiosPublic.js
+++ client/src/hooks/useAxiosPublic.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "https://life-flow-donor-server.vercel.app",
+  baseURL: "http://localhost:5000",
 });
 
 const useAxiosPublic = () => {
@@ -9,5 +9,3 @@ const useAxiosPublic = () => {
 };
 
 export default useAxiosPublic;
-
-
